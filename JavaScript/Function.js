feb9fd7d6e7dfb710d076cc77d43c686b1de907c
sankarimaran2902 <sankarimaran2902@gmail.com>
2024-11-18 10:57:16 +0530

    JavascriptFunctions

---
+++ JavaScript/Function.js
@@ -0,0 +1,13 @@
+function sankari()
+{
+    alert("Welcome function Calling")
+}
+function sum(a,b)
+{
+    let result=a+b;
+    alert("A values is: " +a +" B values is: " +b +" a+b is: " +result )
+}
+let print=(name)=>
+{
+    document.writeln("My name is: "+name)
+}
